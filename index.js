05d3d7c9d8c3484f8f4655d50693c4a39d577503
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-08 16:49:35 +0600

    get-services-mongodb

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require ('dotenv').config();
 
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cez8utx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db('perfect-engineering').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error));
+
 
 
 app.get('/', (req, res) => {

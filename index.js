9f4537838c541c0f2d44e44909868733df7407f8
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-10 02:17:21 +0600

    jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt=require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -13,10 +14,41 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cez8utx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req,res,next){
+    const authHeader=(req.headers.authorization)
+    if(!authHeader){
+        res.status(401).send ({message:'unauthorized access'})
+    }
+    const token=authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err,decoded){
+        if(err){
+            res.status(401).send ({message:'unauthorized access'})
+        }
+        req.decoded=decoded;
+        next();
+
+    })
+}
+
+
+
 async function run() {
     try {
         const serviceCollection = client.db('perfect-engineering').collection('services');
         const commentCollection = client.db('perfect-engineering').collection('comments');
+
+
+        app.post('/jwt', (req, res)=>{
+            const user=req.body;
+            const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+            res.send({token})
+        })
+
+
+
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -48,7 +80,11 @@ async function run() {
           });
 
 
-        app.get('/comments', async (req, res) => {
+        app.get('/comments', verifyJWT, async (req, res) => {
+            const decoded=req.decoded;
+            if(decoded.email !==req.query.email){
+                req.status(403).send({message:'Forbidden access'})
+            }
             let query = {};
             if(req.query.email){
                 query={
